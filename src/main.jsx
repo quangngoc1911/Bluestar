80985b4e24a4cb4ce4ecc0596ce350d5441bb953
Le Quang Ngoc <nhuthung102030@gmail.com>
2024-12-06 15:29:15 +0700

    update delete boostrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'jquery';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'swiper/swiper-bundle.min.js';
 import 'jquery';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'swiper/swiper-bundle.min.js';
 
 createRoot(document.getElementById('root')).render(
