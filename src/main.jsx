60b6cafe7bb10bdc4cf4b79905271298ecb05199
Le Quang Ngoc <nhuthung102030@gmail.com>
2024-12-06 15:30:14 +0700

    update delete jquery

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'jquery';
-import 'swiper/swiper-bundle.min.js';
-import 'jquery';
-import 'swiper/swiper-bundle.min.js';
+
 
 createRoot(document.getElementById('root')).render(
 <App />
